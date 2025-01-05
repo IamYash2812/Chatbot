be3524a7dd2c2f754730dd272339765fb775764b
Yash <yashaoute@gmail.com>
2025-01-05 08:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,22 @@ output_parser = get_output_parser()
 
 chain = prompt_template|llm|output_parser
 
+#chat history
+if "chat_history" not in st.seession_state:
+    st.seession_state["chat_history"]  = []
+
+
 #streamlit framework
 st.title("langchain Demo with ollama")
 input_text =st.text_input("Ask anything...")
 
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
-    
+    response = chain.invoke({"question":input_text})
+    st.session_state['chat_history'].append({"question":input_text,"response":response} )
+    st.write(response)
+
+st.write("Chat History")
+for entry in st.session_state["chat_history"]:
+    st.write(f"You: {entry['question']}")
+    st.write(f"VLMBOT: {entry['response']}")
